2ef23601d15de8f169fc84e44ad10d2c9683eb8d
MohamadSaif <mood2006saif@gmail.com>
2026-01-25 21:34:43 +0300

    add exam-config.js file

---
+++ js/exam-config.js
@@ -0,0 +1,74 @@
+const EXAM_CONFIG = [
+  {
+    "id": "java1",
+    "name": "Java 1",
+    "description": "Introduction to Java",
+    "path": "data/java1",
+    "chapters": []
+  },
+  {
+    "id": "java2",
+    "name": "Java 2",
+    "description": "Advanced Java",
+    "path": "data/java2",
+    "chapters": [
+      {
+        "id": "1",
+        "name": "Introduction to Computers, Programs, and Java",
+        "q": 45,
+        "file": "data/java2/chapter1.json"
+      },
+      {
+        "id": "10",
+        "name": "Object-Oriented Thinking",
+        "q": 47,
+        "file": "data/java2/chapter10.json"
+      },
+      {
+        "id": "11",
+        "name": "Inheritance and Polymorphism",
+        "q": 65,
+        "file": "data/java2/chapter11.json"
+      },
+      {
+        "id": "12",
+        "name": "Exception Handling and Text I/O",
+        "q": 48,
+        "file": "data/java2/chapter12.json"
+      },
+      {
+        "id": "13",
+        "name": "Abstract Classes and Interfaces",
+        "q": 35,
+        "file": "data/java2/chapter13.json"
+      },
+      {
+        "id": "17",
+        "name": "Binary I/O",
+        "q": 20,
+        "file": "data/java2/chapter17.json"
+      }
+    ]
+  },
+  {
+    "id": "algorithm",
+    "name": "Algorithms",
+    "description": "Algorithm Analysis",
+    "path": "data/algorithm",
+    "chapters": []
+  },
+  {
+    "id": "data_structure",
+    "name": "Data Structures",
+    "description": "Data Structures",
+    "path": "data/data_structure",
+    "chapters": []
+  },
+  {
+    "id": "java_advanced",
+    "name": "Java Advanced",
+    "description": "Enterprise Java",
+    "path": "data/java_advanced",
+    "chapters": []
+  }
+];
