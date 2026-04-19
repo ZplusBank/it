99e013de4321d55d0517d180859787b53c7058cb
ZplusBank <256682235+ZplusBank@users.noreply.github.com>
2026-04-19 21:19:11 +0300

    edit mid math network name

--- js/exam-config.js
+++ js/exam-config.js
@@ -440,7 +440,7 @@ const EXAM_CONFIG = [
       },
       {
         "id": "8",
-        "name": "Mid-mathbased by Z+",
+        "name": "Mid - MATH by Z+",
         "q": 37,
         "file": "data/Network/midmath.json"
       }
